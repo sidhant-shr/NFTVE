87341740dba245de4222553f5baa1145ead4637d
Sidhant <shr.sidhant@gmail.com>
2023-06-14 11:37:44 +0530

    clrf issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react"
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import Appp from './components/appp'
+import Appp from './components/appp';
 
 ReactDOM.render(<Appp />, document.getElementById('root'));
